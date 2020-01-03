af92c2451765df7a389b224eff4a2dc65495085c
Yuchi.Sun <sunyuchi_@live.com>
2020-01-03 11:22:43 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def function1(x):
+    return x+1
+
+
+def function2(x):
+    return x+2
+
+
+if __name__ == '__main__':
+    print('function1 output', function1(0))
+    print('function2 output', function2(0))
