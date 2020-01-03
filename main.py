d69ef0ac42580551e492bb10b18083fd8069f200
Yuchi.Sun <sunyuchi_@live.com>
2020-01-03 14:10:07 -0800

    Add function 4

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def function3(x):
     return x+3
 
 
+def function4(x):
+    return x+4
+
+
 if __name__ == '__main__':
     print('function1 output', function1(0))
     print('function2 output', function2(0))
     print('function3 output', function3(0))
+    print('function4 output', function4(0))
