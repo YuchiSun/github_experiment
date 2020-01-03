736915b819c2f8de81fd606fe9e156e9273e08fd
Yuchi.Sun <sunyuchi_@live.com>
2020-01-03 11:28:41 -0800

    Add function 3

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def function2(x):
     return x+2
 
 
+def function3(x):
+    return x+3
+
+
 if __name__ == '__main__':
     print('function1 output', function1(0))
     print('function2 output', function2(0))
+    print('function3 output', function3(0))
